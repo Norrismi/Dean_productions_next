138717bfec6e6b3001ad3ce78ce15fafdc00871c
Norrismi <Norrismi0@gmail.com>
2021-07-28 15:02:20 -0400

    first commit

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,24 @@
+
+import Head from "next/head";
+import Layout from '../components/Layout'
 import '../styles/globals.css'
 
+// add bootstrap css 
+import 'bootstrap/dist/css/bootstrap.css'
+
+
+
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <Head>
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+      </Head>
+      <Layout>
+        <Component {...pageProps} />
+      </Layout>
+    </>
+  )
 }
 
 export default MyApp
